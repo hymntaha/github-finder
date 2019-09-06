ec5c4e32df5afb61ab124b5d88464361464cc9cd
taha uygun <tahauygun@gmail.com>
2019-09-05 23:25:17 -0700

    setting up prop types

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
-class App extends Component() {
+
+
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello from react</h1>
+        <Navbar/>
       </div>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Navbar extends Component {
 
   static defaultProps = {
     title:'Github Finder',
     icon: ' fab fa-github'
+  }
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
   }
   render() {
     return (
