aaeac95000e7bb6a4f8e3181ff8a1f640dcdd33e
taha uygun <tahauygun@gmail.com>
2019-09-05 23:39:18 -0700

    get rid of constructor from UserItem

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
+  state = {
       id: 'id',
       login: 'mojombo',
       avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-      html_user: 'https://github.com/mojombo',
-    };
-  }
+      html_url: 'https://github.com/mojombo',
+  };
+
   render() {
     return (
       <div className="card text-center">
