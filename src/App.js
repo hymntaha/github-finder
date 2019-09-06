4b85d6a9ce1022225e7d2d3bac04d0c70016d33c
taha uygun <tahauygun@gmail.com>
2019-09-05 23:32:43 -0700

    adding user item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar/>
+        <UserItem/>
       </div>
     );
   }
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class UserItem extends Component {
+  render() {
+    return (
+      <div>
+        UserItem
+      </div>
+    );
+  }
+}
+
+export default UserItem;
