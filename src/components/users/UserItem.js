b0243e84a40e82ef940b61f6d6bf2c6af62dc107
taha uygun <tahauygun@gmail.com>
2019-09-05 23:38:00 -0700

    adding github user api

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class UserItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 'id',
+      login: 'mojombo',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+      html_user: 'https://github.com/mojombo',
+    };
+  }
   render() {
     return (
-      <div>
-        UserItem
+      <div className="card text-center">
+        <img
+          src={this.state.avatar_url}
+          alt=""
+          className="round-img"
+          style={{ width: '60px' }}
+        />
+        <h3>{this.state.login}</h3>
+        <div>
+          <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+            More
+          </a>
+        </div>
       </div>
     );
   }
