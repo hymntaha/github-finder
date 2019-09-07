58863ce3560de5871ae22b58ccf01e1dc4432b59
taha uygun <tahauygun@gmail.com>
2019-09-06 22:52:07 -0700

    addin axios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/USers';
+import axios from 'axios';
 import './App.css';
 
-
-
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount() {
+    this.state({loading: true});
+
+    const res = await axios
+      .get('https://api.github.com/users')
+
+    this.state({user: res.data, loading: false})
+
+  }
+
   render() {
     return (
       <div className="App">
-        <Navbar/>
+        <Navbar />
         <div className="container">
-          <Users/>
+          <Users />
         </div>
       </div>
     );
