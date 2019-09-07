cc3b4f9e4858511277b479ab5500284cd34d5503
taha uygun <tahauygun@gmail.com>
2019-09-06 22:40:25 -0700

    style stuff

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar/>
-        <Users/>
+        <div className="container">
+          <Users/>
+        </div>
       </div>
     );
   }
