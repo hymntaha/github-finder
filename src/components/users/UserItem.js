b6ae96c49bfe879682206918c7a8b71f7c41d5aa
taha uygun <tahauygun@gmail.com>
2019-09-06 22:26:46 -0700

    some trimming

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -9,17 +9,18 @@ class UserItem extends Component {
   };
 
   render() {
+    const {login, avatar_url, html_url} = this.state;
     return (
       <div className="card text-center">
         <img
-          src={this.state.avatar_url}
+          src={avatar_url}
           alt=""
           className="round-img"
           style={{ width: '60px' }}
         />
-        <h3>{this.state.login}</h3>
+        <h3>{login}</h3>
         <div>
-          <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
             More
           </a>
         </div>
