19d304e00874a25416ab0e43abfa3a4929ee4765
taha uygun <tahauygun@gmail.com>
2019-09-08 22:48:34 -0700

    adding form error

--- src/App.js
+++ src/App.js
@@ -8,28 +8,42 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    loading: false
-  }
+    loading: false,
+    alert: null,
+  };
 
-  searchUsers = async text =>{
-    this.state({loading: true});
+  searchUsers = async text => {
+    this.state({ loading: true });
 
-    const res = await axios
-      .get(`https://api.github.com/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    const res = await axios.get(
+      `https://api.github.com/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+    );
 
-    this.state({user: res.data, loading: false})
-  }
+    this.state({ user: res.data, loading: false });
+  };
 
-  clearUsers = () => this.setState({users: [], loading: false})
+  clearUsers = () => this.setState({ users: [], loading: false });
 
+  setAlert = (msg, type) => {
+    this.setState({
+      alert: { msg, type },
+    });
+  };
   render() {
-    const { users, loading} = this.state;
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUser={this.searchUsers} clearUsers={this.clearUsers} showClear={ users.length > 0 ? true: false}/>
-          <Users loading={loading} users={users}/>
+          <Search
+            searchUser={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
