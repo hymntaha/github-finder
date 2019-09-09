2f41324395201706eac5d43f3d85f2d39dea8ea6
taha uygun <tahauygun@gmail.com>
2019-09-08 22:44:24 -0700

    some refactoring

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,34 +1,48 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Search extends Component {
   state = {
-    text:''
-  }
+    text: '',
+  };
 
-  onChange = (e) => this.setState({[e.target.name]: e.target.value})
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
-  }
+  };
 
-  onSubmit = (e) => {
+  onSubmit = e => {
     e.preventDefault();
     this.props.searchUsers(this.state.text);
-    this.setState({text: ''})
-  }
+    this.setState({ text: '' });
+  };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
-          <input type="text" placeholder="Search Users..." name="text" value={this.state.text} onChange={this.onChange}/>
-          <input type="submit" value="Search" className="btn btn-dark btn-block"/>
+          <input
+            type="text"
+            placeholder="Search Users..."
+            name="text"
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
         </form>
-        {this.props.showClear && <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>}
-
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
