6e9311fd23ec3aecbf391f1a5bac560897ad178a
taha uygun <tahauygun@gmail.com>
2019-09-08 22:29:31 -0700

    adding events for search

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 
 class Search extends Component {
+  state = {
+    text:''
+  }
+
+  onChange = (e) => {
+    this.setState({text: e.target.value})
+  }
+
   render() {
     return (
       <div>
         <form  className="form">
-          <input type="text" placeholder="Search Users..." name="text"/>
+          <input type="text" placeholder="Search Users..." name="text" value={this.state.text} onChange={this.onChange}/>
           <input type="submit" value="Search" className="btn btn-dark btn-block"/>
         </form>
       </div>
