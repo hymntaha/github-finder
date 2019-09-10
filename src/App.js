2c17ff2cb78ecc9a43ef11a85b9aace62673619b
taha uygun <tahauygun@gmail.com>
2019-09-09 20:19:53 -0700

    adding about pag

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import About from './components/pages/About';
 
 import axios from 'axios';
 import './App.css';
@@ -59,6 +60,7 @@ class App extends Component {
                   </Fragment>
                 )}
               />
+              <Route exact path="/about" component={About} />
             </Switch>
           </div>
         </div>
