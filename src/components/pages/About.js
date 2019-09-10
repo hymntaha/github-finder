da003358e1100e94df80a8cda00a67aeb40c6fe0
taha uygun <tahauygun@gmail.com>
2019-09-09 20:18:49 -0700

    adding about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const MyComponent = () => {
+  return (
+    <Fragment>
+      <h1>About this app</h1>
+      <p>App the search Github users</p>
+      <p>Version 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default MyComponent;
