8aa867e6b7be78dd546d5ced81926865c1493dd0
taha uygun <tahauygun@gmail.com>
2019-09-14 22:17:56 -0700

    starting github state

---
+++ src/components/context/github/GithubState.js
@@ -0,0 +1,22 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USER,
+  GET_USER,
+  GET_REPOS,
+} from '../types';
+
+const GithubState = props => {
+  const initialSTate = {
+    users: [],
+    user: {},
+    repos: {},
+    loading: false,
+  };
+  const [state, dispatch] = useReducer(GithubReducer, initialSTate);
+};
