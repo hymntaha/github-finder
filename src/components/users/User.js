a5bd910b5aad007c030a7eb06ce90069677dd8de
taha uygun <tahauygun@gmail.com>
2019-09-16 22:55:09 -0700

    user repos fixes

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -5,9 +5,10 @@ import { Link } from 'react-router-dom';
 import Repos from '../repos/Repos';
 import GithubContext from '../../context/github/githubContext';
 
-const User = ({ getUserRepos, repos, match }) => {
+const User = ({ match }) => {
   const githubContext = useContext(GithubContext);
-  const { getUser, loading, user } = githubContext;
+
+  const { getUser, loading, user, repos, getUserRepos } = githubContext;
 
   useEffect(() => {
     getUser(match.params.login);
@@ -104,8 +105,4 @@ const User = ({ getUserRepos, repos, match }) => {
   );
 };
 
-User.propTypes = {
-  repos: PropTypes.array.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
-};
 export default User;
