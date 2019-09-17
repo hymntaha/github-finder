af4bebdd723dbea8595c19b53b3a47d3ffc90b9b
taha uygun <tahauygun@gmail.com>
2019-09-16 22:53:55 -0700

    useContext conversion continues

--- src/components/context/github/GithubState.js
+++ src/components/context/github/GithubState.js
@@ -50,6 +50,18 @@ const GithubState = props => {
     });
   };
 
+  const getUserRepos = async username => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users?q=${username}/repos?per_page=5&sort=created:asc?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+    );
+
+    dispatch({ type: GET_REPOS, payload: res.data });
+  };
+
   const clearUsers = () => dispatch({ type: CLEAR_USER });
 
   const setLoading = () => dispatch({ type: SET_LOADING });
@@ -64,6 +76,7 @@ const GithubState = props => {
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {props.children}
